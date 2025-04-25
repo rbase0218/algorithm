6acc4a85f91047fcd8921a3f3654b6265a8f90e5
Hyeon <44202628+rbase0218@users.noreply.github.com>
2025-04-25 16:19:45 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    while (n > 0)
+    {
+        cout << "SciComLove" << endl;
+        n--;
+    }
+
+    return 0;
+}
