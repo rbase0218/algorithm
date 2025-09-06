599b33255fb4fe6e30334013fb09610cefb13aac
Hyeon <44202628+rbase0218@users.noreply.github.com>
2025-09-06 20:50:39 +0900

    [Silver IV] Title: 로프, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,13 +1,20 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <queue>
 #include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <set>
+
 using namespace std;
 
 int main() {
 	int n, ans;
 	cin >> n;
 	vector<int> rope(n, 0);
+
 	for (int i = 0; i < n; ++i) {
 		cin >> rope[i];
 	}
@@ -15,9 +22,12 @@ int main() {
 	sort(rope.begin(), rope.end());
 	ans = rope[n - 1];
 
-	for (int i = n - 1; i >= 0; --i) {
+	for (int i = n - 1; i >= 0; --i) 
+	{
 		if (ans < rope[i] * (n - i))
+		{
 			ans = rope[i] * (n - i);
+		}
 	}
 
 	cout << ans;
