296419156b38071ae889a203210ed24c4ca1d021
Hyeon <44202628+rbase0218@users.noreply.github.com>
2026-02-23 17:57:06 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -15,18 +15,30 @@ int main(void)
         if (text[i] == '0') zero++;
         else one++;
     }
-    
-    zero /= 2;
-    one /= 2;
 
-    string newText = "";
-    for (int i = 0; i < zero + one; ++i)
-    {
-        if (i < zero) newText += '0';
-        else newText += '1';
+    zero *= 0.5;
+    one *= 0.5;
+    
+    for (int i = 0; i < text.size(); ++i) {
+        if (zero != 0 && text[text.size() - (i + 1)] == '0')
+        {
+            text[text.size() - (i + 1)] = '9';
+            zero--;
+        }
+        if (one != 0 && text[i] == '1')
+        {
+            text[i] = '9';
+            one--;
+        }
     }
 
-    cout << newText;
+    for (int i = 0; i < text.size(); ++i)
+    {
+        if (text[i] != '9')
+        {
+            cout << text[i];
+        }
+    }
 
     return 0;
 }
